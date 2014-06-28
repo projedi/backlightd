ba13e531eae4eddd991414835fa9daa16a17752c
Alexander Shabalin <shabalyn.a@gmail.com>
2014-06-28 21:43:46 +0400

    Reformulate in terms of BACKLIGHT_LEVEL_COUNT instead of BACKLIGHT_STEP

--- Makefile
+++ Makefile
@@ -5,5 +5,4 @@ setuid-root: backlight_helper
 	sudo chmod u+s backlight_helper
 
 backlight_helper: main.c
-	gcc -std=c99 -Wall -Wextra main.c -o backlight_helper
-
+	gcc -std=c99 -Wall -Wextra main.c -o backlight_helper -lm
--- main.c
+++ main.c
@@ -1,9 +1,37 @@
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+#define clamp(v, vmin, vmax) ((v) > (vmax) ? (vmax) : (v) < (vmin) ? (vmin) : (v))
+
 #define BACKLIGHT_PATH "/sys/class/backlight/intel_backlight"
-#define BACKLIGHT_STEP 80
+#define BACKLIGHT_LEVEL_COUNT 10
+
+// level is clamped between 0 and 1.
+// returns a value between 0 and 1.
+double backlight_function(double level) {
+	double x = clamp(level, 0, 1);
+	double y = x;
+	return clamp(y, 0, 1);
+}
+
+int get_backlight_value(int current_level, int max_val) {
+	int l = clamp(current_level, 0, BACKLIGHT_LEVEL_COUNT - 1);
+	return (int)round(backlight_function((double)l / (BACKLIGHT_LEVEL_COUNT - 1)) * max_val);
+}
+
+// I assume that BACKLIGHT_LEVEL_COUNT is low
+int get_backlight_level(int current_val, int max_val) {
+	int level;
+	int diff = abs(get_backlight_value(0, max_val) - current_val);
+	for(level = 0; level != BACKLIGHT_LEVEL_COUNT; ++level) {
+		int cur_diff = abs(get_backlight_value(level, max_val) - current_val);
+		if(cur_diff > diff) break;
+		diff = cur_diff;
+	}
+	return clamp(level - 1, 0, BACKLIGHT_LEVEL_COUNT - 1);
+}
 
 void usage(char const* program_name) {
 	printf("USAGE: %s {up|down}\n", program_name);
@@ -42,15 +70,12 @@ void backlight_write(int new_val) {
 	fclose(cur_file);
 }
 
-int clamp(int v, int vmin, int vmax) {
-	return v > vmax ? vmax : v < vmin ? vmin : v;
-}
-
 void backlight_up() {
 	int current_val;
 	int max_val;
 	backlight_read(&current_val, &max_val);
-	int new_val = clamp(current_val + BACKLIGHT_STEP, 0, max_val);
+	int current_level = get_backlight_level(current_val, max_val);
+	int new_val = get_backlight_value(current_level + 1, max_val);
 	backlight_write(new_val);
 }
 
@@ -58,7 +83,8 @@ void backlight_down() {
 	int current_val;
 	int max_val;
 	backlight_read(&current_val, &max_val);
-	int new_val = clamp(current_val - BACKLIGHT_STEP, 0, max_val);
+	int current_level = get_backlight_level(current_val, max_val);
+	int new_val = get_backlight_value(current_level - 1, max_val);
 	backlight_write(new_val);
 }
 
