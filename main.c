fb5597788bc6ac62b8ffdba478ca892e00399c35
Alexander Shabalin <shabalyn.a@gmail.com>
2014-06-29 01:46:57 +0400

    Add listening to acpid to restore backlight after plug/unplug

--- main.c
+++ main.c
@@ -3,14 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/un.h>
 #include <unistd.h>
 
 #include "backlight.h"
 #include "io.h"
 
 #define FIFO_PATH "/tmp/backlightd.fifo"
+#define ACPID_SOCKET "/var/run/acpid.socket"
+
+static int CURRENT_BACKLIGHT_VALUE = -1;
 
 void backlight_up() {
 	int current_val;
@@ -18,6 +23,7 @@ void backlight_up() {
 	backlight_read(&current_val, &max_val);
 	int current_level = get_backlight_level(current_val, max_val);
 	int new_val = get_backlight_value(current_level + 1, max_val);
+	CURRENT_BACKLIGHT_VALUE = new_val;
 	backlight_write(new_val);
 }
 
@@ -27,7 +33,18 @@ void backlight_down() {
 	backlight_read(&current_val, &max_val);
 	int current_level = get_backlight_level(current_val, max_val);
 	int new_val = get_backlight_value(current_level - 1, max_val);
+	CURRENT_BACKLIGHT_VALUE = new_val;
 	backlight_write(new_val);
+}
+
+void backlight_save() {
+	int max_val;
+	backlight_read(&CURRENT_BACKLIGHT_VALUE, &max_val);
+}
+
+void backlight_restore() {
+	if(CURRENT_BACKLIGHT_VALUE == -1) return;
+	backlight_write(CURRENT_BACKLIGHT_VALUE);
 }
 
 int open_fifo() {
@@ -48,8 +65,70 @@ int open_fifo() {
 	return open(FIFO_PATH, O_RDONLY);
 }
 
+void handle_acpi_event(char const* buf) {
+	char copy_buf[4097];
+	strncpy(copy_buf, buf, sizeof(copy_buf));
+	char const* type = copy_buf;
+	char const* name = NULL;
+	char const* data1 = NULL;
+	char const* data2 = NULL;
+	char const** splits[] = { &name, &data1, &data2 };
+	int on_splitter = 0;
+	int split_idx = 0;
+	char* sbuf = copy_buf;
+	for(; *sbuf; ++sbuf) {
+		if(*sbuf == ' ' || *sbuf == '\t') {
+			on_splitter = 1;
+			*sbuf = 0;
+			continue;
+		}
+		if(on_splitter)
+			*splits[split_idx++] = sbuf;
+		on_splitter = 0;
+	}
+	if(split_idx != 3) return;
+	if(!strcmp(type, "ac_adapter")) {
+		backlight_restore();
+	}
+}
+
+void acpi_listen() {
+	int ret = fork();
+	if(ret == -1) {
+		perror("Cannot fork");
+		exit(EXIT_FAILURE);
+	}
+	if(ret) return;
+	int s = socket(AF_UNIX, SOCK_STREAM, 0);
+	if(s == -1) {
+		perror("Cannot open socket");
+		exit(EXIT_FAILURE);
+	}
+	struct sockaddr_un addr;
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, ACPID_SOCKET, sizeof(addr.sun_path) - 1);
+	if(connect(s, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+		perror("Cannot connect to acpid socket " ACPID_SOCKET);
+		exit(EXIT_FAILURE);
+	}
+	char buf[4097];
+	for(;;) {
+		int len = read(s, buf, sizeof(buf) - 1);
+		if(len == -1 && errno != EAGAIN) {
+			perror("Cannot read from ACPID_SOCKET " ACPID_SOCKET);
+			exit(EXIT_FAILURE);
+		}
+		if(len <= 0) continue;
+		buf[len] = 0;
+		handle_acpi_event(buf);
+	}
+}
+
 int main() {
+	backlight_save();
 	umask(0);
+	acpi_listen();
 	for(;;) {
 		int fifo = open_fifo();
 		int v = 0;
